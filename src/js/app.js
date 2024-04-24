c5f71caba57c10ccf0d7929c771d0c1e266fbd26
Maurof18 <maurof1809gmail.com>
2024-04-24 08:35:04 -0300

    .

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,27 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function darkMode(){
+    //cambia de modo oscuro a claro segun como este config el navegador 
+    //solo funciono con edge, con chrome no realiza la accion REVISAR
+    /**
+    const prefiereDarkMode= window.matchMedia('(prefers-color-scheme: dark)');
+    console.log(prefiereDarkMode.matches);
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    }else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change',function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+    })
+     */
     const botonDarkMode= document.querySelector('.dark-mode-boton');
+
     botonDarkMode.addEventListener('click',function(){
         document.body.classList.toggle('dark-mode');
     });
